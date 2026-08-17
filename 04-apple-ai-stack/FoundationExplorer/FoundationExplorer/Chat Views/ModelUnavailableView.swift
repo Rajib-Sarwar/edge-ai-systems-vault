50dc3ec7c5b72b534e19c07d9abf2e42a0552423
Chowdhury Md Rajib Sarwar <rajib@limosys.com>
2026-08-17 16:28:15 -0400

    added reasoning for edge cases to support foundation models

--- 04-apple-ai-stack/FoundationExplorer/FoundationExplorer/Chat Views/ModelUnavailableView.swift
+++ 04-apple-ai-stack/FoundationExplorer/FoundationExplorer/Chat Views/ModelUnavailableView.swift
@@ -4,10 +4,10 @@
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -15,7 +15,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -31,19 +31,22 @@
 import SwiftUI
 import FoundationModels
 
-private let model = SystemLanguageModel.default
+struct ModelUnavailableView: View {
+  
+  var reason: SystemLanguageModel.Availability.UnavailableReason
 
-struct ContentView: View {
   var body: some View {
-    switch model.availability {
-    case .available:
-      ChatView()
-    case .unavailable(let reason):
-      ModelUnavailableView(reason: reason)
+    Image(systemName: "apple.intelligence")
+      .font(.largeTitle)
+    switch reason {
+    case .deviceNotEligible:
+      Text("Apple Intelligence is not available on this device.")
+    case .appleIntelligenceNotEnabled:
+      Text("Apple Intelligence is available, but not enabled on this device.")
+    case .modelNotReady:
+      Text("The model isn't ready. This is usually because it is still downloading.")
+    @unknown default:
+      Text("An unknown error prevents Apple Intelligence from working.")
     }
   }
-}
-
-#Preview {
-  ContentView()
 }
